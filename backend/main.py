3a3528b46b024d74dac828572f3760fbe9156c6f
Sarvami <sarvamiagarwal@gmail.com>
2026-04-09 22:44:45 +0530

    bro idk what it fixed bro

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,9 @@ STT_POLL_INTERVAL = 1
 
 # ------------------ DB CONNECTION ------------------
 
-DB_PATH = "data/hospital.db"
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "data", "hospital.db")
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
